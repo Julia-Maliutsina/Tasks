caec72a02e82ee53e151b39df23d878e2480a134
Julia <89554520+Julia-Maliutsina@users.noreply.github.com>
2021-10-13 14:01:56 +0300

    Delete task1 directory

--- task1/getUniqueNumber.js
+++
@@ -1,9 +0,0 @@
-function getUnique(array) {
-  for (let i = 0; i < array.length; i++) {
-    if (array.indexOf(array[i]) == array.lastIndexOf(array[i])) {
-      return array[i];
-    }
-  }
-}
-const TEST_ARRAY = [1, 1, 1, 2, 2, 3, 4, 4, 5, 5, 5, 5];
-const uniqueElement = getUnique(TEST_ARRAY);
