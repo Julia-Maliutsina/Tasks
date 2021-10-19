ead764c3e0479043b116f22044c44e713be4a332
Julia-Maliutsina <julia.maliutsina@gmail.com>
2021-10-19 15:25:15 +0300

    useState

--- notes/src/components/renderNote.jsx
+++ notes/src/components/renderNote.jsx
@@ -4,22 +4,27 @@ import ListItemText from "@mui/material/ListItemText"
 import PropTypes from "prop-types"
 
 function Note(props) {
+
+    function f(){
+        const id = props.note.id;
+        const activeNote = document.getElementsByClassName("chosenNote")[0];
+        const noteText = props.note.text;
+        activeNote.innerHTML= noteText;
+        const allNotes = document.getElementsByTagName('li');
+        for (let n=0; n<allNotes.length; n++) {
+            allNotes[n].style.background="inherit";
+        }
+        const noteInList = allNotes[id];
+        noteInList.style.background= "#ccaabe";
+    }
+
+    const [notes, setNotes] = React.useState(props.note);
+
     return( 
         <ListItem divider>
             <ListItemText 
             primary={props.note.text.slice(0,20) + "..."} 
-            onClick={()=>{
-                const id = props.note.id;
-                const activeNote = document.getElementsByClassName("chosenNote")[0];
-                const noteText = props.note.text;
-                activeNote.innerHTML= noteText;
-                const allNotes = document.getElementsByTagName('li');
-                for (let n=0; n<allNotes.length; n++) {
-                    allNotes[n].style.background="inherit";
-                }
-                const noteInList = allNotes[id];
-                noteInList.style.background= "#ccaabe";
-            }}/>
+            onClick={f}/>
         </ListItem>
     )
 }
