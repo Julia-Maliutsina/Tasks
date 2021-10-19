726e8bd0c1024caa53f1afa7a28da5afc387d3cf
Julia-Maliutsina <julia.maliutsina@gmail.com>
2021-10-19 14:32:29 +0300

    added functions, structure changed

--- notes/src/components/renderNotesList.jsx
+++ notes/src/components/renderNotesList.jsx
@@ -1,24 +1,27 @@
 import React from "react"
 import List from "@mui/material/List"
 import Note from "./renderNote"
-import styles from "../styled"
+import styles from "../pages/styled"
 import PropTypes from "prop-types"
+import { ListItem } from "@mui/material"
+
+
 
 function Notes(props) {
     return (
         <div style={styles.AllNotes}>
             <List>
                 {props.notes.map(note =>{
-                return <Note note={note}/>
-                }
-                    )}
+                    return <Note note={note} clickNote={props.noteChosen}/>
+                })}
             </List>
         </div>
     );
 }
 
 Notes.propTypes = {
-    notes: PropTypes.arrayOf(PropTypes.object)
+    notes: PropTypes.arrayOf(PropTypes.object),
+    noteChosen: PropTypes.func
 }
 
 export default Notes
