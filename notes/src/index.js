757c47f1650bf02394d99810b28b7380a41315b4
Julia <89554520+Julia-Maliutsina@users.noreply.github.com>
2021-10-21 12:56:54 +0300

    updated index.js

--- notes/src/index.js
+++ notes/src/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import App from "./pages/myNotesContainer"
+import App from "./pages/myNotes"
 import reportWebVitals from "./reportWebVitals"
 
 ReactDOM.render(
